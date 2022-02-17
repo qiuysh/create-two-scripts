b47cdde05093b61e7059c98feccec4744e9c6f90
wulin <wulin@dtstack.com>
2022-02-17 13:36:46 +0800

    feat: update devServer config

--- config/dev.js
+++ config/dev.js
@@ -1,17 +1,42 @@
 /** @format */
 
 const { merge } = require("webpack-merge");
-const webpack = require("webpack");
-const getWebpackBase = require("./base");
+const paths = require("./defaultPaths");
+const getWebpackBaseConfig = require("./base");
 
 module.exports = function (program) {
-  const baseConfig = getWebpackBase(program),
-    { plugins } = baseConfig;
+  // default webpack config
+  const webpackBaseConfig = getWebpackBaseConfig(program),
+    // user custom webpack config
+    appUserConf = paths.getUserConf(),
+    // devServer config
+    serverConf = {
+      allowedHosts: "auto",
+      client: {
+        logging: "info",
+        overlay: {
+          errors: true,
+          warnings: false,
+        },
+        progress: true,
+      },
+      compress: true,
+      contentBase: paths.appDirectory,
+      host: "0.0.0.0",
+      historyApiFallback: true,
+      open: true,
+      port: 3001,
+      // static: paths.appDirectory,
+    },
+    webpackDevConfig = {
+      mode: "development",
+      devtool: "cheap-module-source-map",
+      devServer: serverConf,
+    };
 
-  // 配置合并 开发环境下 publicPath 指定为 / 与 webpack server 一致
-  return merge(baseConfig, {
-    mode: "development",
-    devtool: "cheap-module-source-map",
-    plugins,
+  // 配置合并
+  return merge(webpackBaseConfig, {
+    ...webpackDevConfig,
+    ...appUserConf,
   });
 };
