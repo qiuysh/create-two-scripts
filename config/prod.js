430f6a14942cd456e978cbc7b5a14930db0903cd
qiuyshu <qiuyshu@163.com>
2022-02-20 20:04:23 +0800

    feat: update init logic

--- config/prod.js
+++ config/prod.js
@@ -12,71 +12,67 @@ module.exports = function (program) {
   const webpackBaseConfig = getWebpackBase(program),
     // user custom webpack config
     appUserConf = getUserConf(),
-    // js mini
-    jsMini = new TerserPlugin({
-      parallel: true,
-      terserOptions: {
-        compress: {
-          drop_console: true,
-          drop_debugger: true,
-        },
-        format: {
-          comments: false,
-        },
-      },
-      extractComments: false,
-    }),
-    // css mini
-    cssMini = new CssMinimizerPlugin(),
     // split plugin
-    splitPlugin = new webpack.optimize.SplitChunksPlugin({
-      // webpack 5.x 推荐使用默认配置或使用 optimization.splitChunks: { chunks: 'all' } 配置；
-      chunks: "all",
-      // （默认值：30000s）块的最小大小
-      minSize: 30000,
-      maxSize: 600000,
-      minRemainingSize: 0,
-      minChunks: 1,
-      maxAsyncRequests: 30,
-      maxInitialRequests: 30,
-      enforceSizeThreshold: 50000,
-      cacheGroups: {
-        antd: {
-          name: "antd",
-          test: /[\\/]node_modules[\\/]antd[\\/]/,
-          chunks: "initial",
-        },
-        lodash: {
-          name: "lodash",
-          test: /[\\/]node_modules[\\/]lodash[\\/]/,
-          chunks: "initial",
-          // 默认组的优先级为负数，以允许任何自定义缓存组具有更高的优先级（默认值为0）
-          priority: -10,
-        },
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/](react|react-dom|react-redux|react-router-dom|moment|js-cookie)[\\/]/,
-          name: "default-vendors",
-          chunks: "all",
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
-        },
-      },
-    }),
     { plugins = [], optimization } = webpackBaseConfig;
 
-  if (jsMini) {
+  if (Array.isArray(optimization?.minimizer)) {
     optimization.minimize = true;
-    optimization.minimizer.push(jsMini);
-  }
-
-  if (cssMini) {
+    const jsTerserPlugin = new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          compress: {
+            drop_console: true,
+            drop_debugger: true,
+          },
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+      cssMini = new CssMinimizerPlugin();
+    optimization.minimizer.push(jsTerserPlugin);
     optimization.minimizer.push(cssMini);
   }
 
-  if (splitPlugin) {
+  if (Array.isArray(plugins)) {
+    const splitPlugin =
+      new webpack.optimize.SplitChunksPlugin({
+        // webpack 5.x 推荐使用默认配置或使用 optimization.splitChunks: { chunks: 'all' } 配置；
+        chunks: "all",
+        // （默认值：30000s）块的最小大小
+        minSize: 30000,
+        maxSize: 600000,
+        minRemainingSize: 0,
+        minChunks: 1,
+        maxAsyncRequests: 30,
+        maxInitialRequests: 30,
+        enforceSizeThreshold: 50000,
+        cacheGroups: {
+          antd: {
+            name: "antd",
+            test: /[\\/]node_modules[\\/]antd[\\/]/,
+            chunks: "initial",
+          },
+          lodash: {
+            name: "lodash",
+            test: /[\\/]node_modules[\\/]lodash[\\/]/,
+            chunks: "initial",
+            // 默认组的优先级为负数，以允许任何自定义缓存组具有更高的优先级（默认值为0）
+            priority: -10,
+          },
+          defaultVendors: {
+            test: /[\\/]node_modules[\\/](react|react-dom|react-redux|react-router-dom|moment|js-cookie)[\\/]/,
+            name: "default-vendors",
+            chunks: "all",
+          },
+          default: {
+            minChunks: 2,
+            priority: -20,
+            reuseExistingChunk: true,
+          },
+        },
+      });
     plugins.push(splitPlugin);
   }
 
