68e07845ad2d612f49ad216143ca2bc24041f478
wulin <wulin@dtstack.com>
2022-03-14 14:02:06 +0800

    feat: update template

--- template/src/app.tsx
+++ template/src/app.tsx
@@ -4,20 +4,10 @@ import { ConfigProvider } from "antd";
 import zhCN from "antd/es/locale/zh_CN";
 import RootRouter from "./routers";
 
-const renderApp = (Component: any) => {
-  ReactDOM.render(
-    <ConfigProvider locale={zhCN}>
-      <Component />
-    </ConfigProvider>,
-    document.getElementById("root")
-  )
-};
+ReactDOM.render(
+  <ConfigProvider locale={zhCN}>
+    <RootRouter />
+  </ConfigProvider>,
+  document.getElementById("root")
+)
 
-renderApp(RootRouter);
-
-if ((module as any).hot) {
-  (module as any).hot.accept(['./routers'], () => {
-    const NewRoute = require('./routers').default;
-    renderApp(NewRoute)
-  })
-}
