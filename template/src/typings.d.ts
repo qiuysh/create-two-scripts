c74be451b75080107ececa036df0f5ea91e49a15
wulin <wulin@dtstack.com>
2022-03-25 20:31:45 +0800

    feat: update temp

--- template/src/typings.d.ts
+++ template/src/typings.d.ts
@@ -7,9 +7,7 @@ declare module "classnames";
 declare module "enzyme";
 declare module "enzyme-adapter-react-16";
 declare module "lodash-es";
-declare module "react-test-renderer";
 declare module "immutability-helper-x";
-declare module "@assets/fonts/iconfont.js";
 
 // 全局
 declare namespace GLOBAL {
