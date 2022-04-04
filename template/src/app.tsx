82f0637035990800fdbeebcad3db9ad633e5e50e
qiuyshu <qiuyshu@163.com>
2022-04-04 17:31:18 +0800

    feat: update template

--- template/src/app.tsx
+++ template/src/app.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ConfigProvider } from "antd";
 import zhCN from "antd/es/locale/zh_CN";
+import "antd/dist/antd.css";
 import RootRouter from "./routers";
 
 ReactDOM.render(
