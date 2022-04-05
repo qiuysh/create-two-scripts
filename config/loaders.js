68bc745c40a97fa2ccb0fc6294c4433c37f72eac
qiuyshu <qiuyshu@163.com>
2022-04-05 19:40:38 +0800

    feat: add oneOf on loader

--- config/loaders.js
+++ config/loaders.js
@@ -42,67 +42,71 @@ module.exports = function (opts) {
 
   return [
     {
-      test: /\.(j|t)s[x]?$/,
-      include: paths.appSrc,
-      use: [
-        esbuild ? esbuildLoader : babelLoader,
+      oneOf: [
         {
-          loader: require.resolve("thread-loader"),
-          options: {
-            workers: 3,
-          },
-        },
-      ],
-    },
-    {
-      test: /\.css$/,
-      exclude: /node_modules\/(!antd)/,
-      use: [StylesLoader, CssLoader, PostcssLoader],
-    },
-    {
-      test: /\.less$/,
-      exclude: /node_modules\/(!antd)/,
-      use: [
-        StylesLoader,
-        CssLoader,
-        {
-          loader: require.resolve("less-loader"), // compiles Less to CSS
-          options: {
-            lessOptions: {
-              javascriptEnabled: true,
+          test: /\.(j|t)s[x]?$/,
+          include: paths.appSrc,
+          use: [
+            esbuild ? esbuildLoader : babelLoader,
+            {
+              loader: require.resolve("thread-loader"),
+              options: {
+                workers: 3,
+              },
             },
-          },
+          ],
         },
-      ],
-    },
-    {
-      test: /\.s[ac]ss$/i,
-      exclude: /node_modules/,
-      use: [
-        StylesLoader,
-        CssLoader,
         {
-          loader: require.resolve("sass-loader"),
-          options: {
-            // Prefer `dart-sass`
-            implementation: require.resolve("sass"),
+          test: /\.css$/,
+          exclude: /node_modules\/(!antd)/,
+          use: [StylesLoader, CssLoader, PostcssLoader],
+        },
+        {
+          test: /\.less$/,
+          exclude: /node_modules\/(!antd)/,
+          use: [
+            StylesLoader,
+            CssLoader,
+            {
+              loader: require.resolve("less-loader"), // compiles Less to CSS
+              options: {
+                lessOptions: {
+                  javascriptEnabled: true,
+                },
+              },
+            },
+          ],
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          exclude: /node_modules/,
+          use: [
+            StylesLoader,
+            CssLoader,
+            {
+              loader: require.resolve("sass-loader"),
+              options: {
+                // Prefer `dart-sass`
+                implementation: require.resolve("sass"),
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(woff|woff2|ttf|eot)$/,
+          type: "asset/resource",
+          generator: {
+            filename: "fonts/[hash][ext][query]",
+          },
+        },
+        {
+          test: /\.(png|jpg|jpeg|gif|webp|svg)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: "images/[hash][ext][query]",
           },
         },
       ],
-    },
-    {
-      test: /\.(woff|woff2|ttf|eot)$/,
-      type: "asset/resource",
-      generator: {
-        filename: "fonts/[hash][ext][query]",
-      },
-    },
-    {
-      test: /\.(png|jpg|jpeg|gif|webp|svg)$/i,
-      type: "asset/resource",
-      generator: {
-        filename: "images/[hash][ext][query]",
-      },
     },
   ];
 };
