6625509e614c56a10d46fc1084e62b2b517ba4e6
wulin <wulin@dtstack.com>
2022-06-17 09:46:37 +0800

    feat: update babel-preset-two-
    app

--- packages/babel-preset-two-app/index.js
+++ packages/babel-preset-two-app/index.js
@@ -1,49 +1,67 @@
 const path = require("path");
 
-module.exports = function (opts) {
-  const { ts, development } = opts;
+/**
+ *
+ * @param {*} context babel context
+ * @param {*} opts custom params
+ * @returns
+ */
+module.exports = function (context, opts = {}) {
+  const { ts } = opts;
+
+  const { NODE_ENV, BABEL_ENV } = process.env;
+
+  const isDev = (NODE_ENV || BABEL_ENV) === "development";
 
   const absoluteRuntimePath = path.dirname(
     require.resolve("@babel/runtime/package.json")
   );
-  return {
-    presets: [
-      [
-        require("@babel/preset-env").default,
-        {
-          useBuiltIns: "entry",
-          corejs: 3,
-          // Exclude transforms that make all code slower
-          exclude: ["transform-typeof-symbol"],
-        },
-      ],
-      [
-        require("@babel/preset-react").default,
-        {
-          development,
-        },
-      ],
-      ts && [require("@babel/preset-typescript").default],
-    ].filter(e => e),
-    plugins: [
-      // support proposal decorators
-      [
-        require("@babel/plugin-proposal-decorators")
-          .default,
-        {
-          legacy: true,
-        },
-      ],
-      [
-        require("@babel/plugin-transform-runtime").default,
-        {
-          corejs: false,
-          version: require("@babel/runtime/package.json")
-            .version,
-          regenerator: true,
-          absoluteRuntime: absoluteRuntimePath,
-        },
-      ],
+
+  const presets = [
+    [
+      require("@babel/preset-env").default,
+      {
+        useBuiltIns: "entry",
+        corejs: 3,
+        // Exclude transforms that make all code slower
+        exclude: ["transform-typeof-symbol"],
+      },
     ],
+    [
+      require("@babel/preset-react").default,
+      {
+        development: isDev,
+      },
+    ],
+  ];
+
+  const plugins = [
+    // support proposal decorators
+    [
+      require("@babel/plugin-proposal-decorators").default,
+      {
+        legacy: true,
+      },
+    ],
+    [
+      require("@babel/plugin-transform-runtime").default,
+      {
+        corejs: false,
+        version: require("@babel/runtime/package.json")
+          .version,
+        regenerator: true,
+        absoluteRuntime: absoluteRuntimePath,
+      },
+    ],
+  ];
+
+  ts &&
+    presets.push([
+      require("@babel/preset-typescript").default,
+    ]);
+
+  return {
+    presets,
+    plugins,
   };
 };
